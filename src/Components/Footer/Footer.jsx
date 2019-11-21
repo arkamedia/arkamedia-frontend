73e4226fee1801530e5c5dcff3513c60f3fb8c1f
afsori <afsoryafsor@gmail.com>
2019-11-21 23:32:26 +0700

    revisi footer selesai

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,38 +15,43 @@ function Footer() {
         <div className="container">
           <div className="row footer-row">
             <div className="col-8">
-              <h5 className="container" style={{ color: "#281e5a" }}>
+              <h5 className="h5" style={{ color: "#281e5a" }}>
                 <br />
                 Jadilah yang pertama tahu berita terbaru event dan penawaran
                 spesial kami
               </h5>
               <form className="form-inline">
-                <div className="form-group mx-sm-3 mb-2">
+                <div className="form-group mx-sm-3 container">
                   <label htmlFor="inputPassword2" className="sr-only">
                     Masukkan Alamat Email Anda
                   </label>
                   <input
-                    style={{ width: "37rem" }}
+                    style={{
+                      width: "35rem",
+                      marginLeft: "-15px",
+                      marginTop: "-2px"
+                    }}
                     type="password"
                     className="form-control"
                     id="inputPassword2"
                     placeholder="Masukkan Alamat Email Anda"
                   />
+                  <button
+                    type="submit"
+                    className="btn btn-primary mb-2"
+                    style={{
+                      width: "5rem",
+                      backgroundColor: "#281e5a",
+                      borderRadius: "3px",
+                      border: "none",
+                      color: "#fff",
+                      fontFamily: "Karla, sans-serif",
+                      marginLeft: "10px"
+                    }}
+                  >
+                    DAFTAR
+                  </button>
                 </div>
-                <button
-                  type="submit"
-                  className="btn btn-primary mb-2"
-                  style={{
-                    width: "6rem",
-                    backgroundColor: "#281e5a",
-                    borderRadius: "3px",
-                    border: "none",
-                    color: "#fff",
-                    fontFamily: "Karla, sans-serif"
-                  }}
-                >
-                  DAFTAR
-                </button>
               </form>
             </div>
             <div className="col-4" style={{ marginTop: "24px" }}>
@@ -90,9 +95,10 @@ function Footer() {
         }}
       >
         <div className="container">
-          <div className="row footer-row">
+          <div className="row footer">
             <div className="col ">
               <h6 className="colom1">BELANJA</h6>
+
               <div>
                 <a href="#!" className="paragraf1">
                   Berbelanja
@@ -151,71 +157,81 @@ function Footer() {
                 </a>
               </div>
             </div>
-            <div className="col xs-4">
-              <h6 className="colom4">PEMBAYARAN</h6>
-              <img
-                className="gambar1"
-                src="https://res.cloudinary.com/afsori/image/upload/v1574187171/Arkamedia/Mandiri_logo_w9jtgx.png"
-                alt="file is loading"
-             ></img>
-              <img
-                className="gambar1"
-                src="https://res.cloudinary.com/afsori/image/upload/v1574187171/Arkamedia/PermataBank_vridau.png"
-                alt="file is loading"
-              ></img>
-              <img
-                className="gambar1"
-                src="https://res.cloudinary.com/afsori/image/upload/v1574213276/Arkamedia/1280px-BNI_logo.svg_mpcvfz.png"
-                alt="file is loading"
-              ></img>
-              <img
-                className="gambar1"
-                src="https://res.cloudinary.com/afsori/image/upload/v1574187169/Arkamedia/BRI_q9jm6o.png"
-                alt="file is loading"
-              ></img>
-              <img
-                className="gambar2"
-                src="https://res.cloudinary.com/afsori/image/upload/v1574187169/Arkamedia/bca_dy5ges.png"
-                alt="file is loading"
-              ></img>
-              <img
-                className="gambar2"
-                src="https://res.cloudinary.com/afsori/image/upload/v1574213276/Arkamedia/Logo_GoPay_Vector_CDR_dan_PNG_bw6pme.png"
-                alt="file is loading"
-              ></img>
-              <h6 className="colom6">PENGIRIMAN</h6>
-              <img
-                className="gambar3"
-                src="https://res.cloudinary.com/afsori/image/upload/v1574213276/Arkamedia/kgx-v_csvq95.png"
-                alt="file is loading"
-             ></img>
-              <img
-                className="gambar3"
-                src="https://res.cloudinary.com/afsori/image/upload/v1574187171/Arkamedia/pickupstore_fp6tz6.png"
-                alt="file is loading"
-              ></img>
-              <img
-                className="gambar3"
-                src="https://res.cloudinary.com/afsori/image/upload/v1574187170/Arkamedia/grab_lgyet8.jpg"
-                alt="file is loading"
-              ></img>
-            </div>
             <div className="col ">
-              <h6 className="colom5">DAPATKAN APPS KAMI</h6>
-              <a href="#!">
+              <h6 className="colom4">PEMBAYARAN</h6>
+              <div className="mandiri">
                 <img
-                  className="app1"
-                  src="https://res.cloudinary.com/afsori/image/upload/v1574187170/Arkamedia/play_yah6hh.png"
-                  alt="file is loading"
-               ></img>
-              </a>
-              <a href="#!">
-                <img
-                  className="app2"
-                  src="https://res.cloudinary.com/afsori/image/upload/v1574187170/Arkamedia/app_store_x0blqd.png"
+                  className="gambar1"
+                  src="https://res.cloudinary.com/afsori/image/upload/v1574187171/Arkamedia/Mandiri_logo_w9jtgx.png"
                   alt="file is loading"
                 ></img>
-              </a>
+                <img
+                  className="gambar1"
+                  src="https://res.cloudinary.com/afsori/image/upload/v1574187171/Arkamedia/PermataBank_vridau.png"
+                  alt="file is loading"
+                ></img>
+                <img
+                  className="gambar1"
+                  src="https://res.cloudinary.com/afsori/image/upload/v1574213276/Arkamedia/1280px-BNI_logo.svg_mpcvfz.png"
+                  alt="file is loading"
+                ></img>
+                <img
+                  className="gambar1"
+                  src="https://res.cloudinary.com/afsori/image/upload/v1574187169/Arkamedia/BRI_q9jm6o.png"
+                  alt="file is loading"
+                ></img>
+              </div>
+              <div className="bca">
+                <img
+                  className="gambar2"
+                  src="https://res.cloudinary.com/afsori/image/upload/v1574187169/Arkamedia/bca_dy5ges.png"
+                  alt="file is loading"
+                ></img>
+                <img
+                  className="gambar2"
+                  src="https://res.cloudinary.com/afsori/image/upload/v1574213276/Arkamedia/Logo_GoPay_Vector_CDR_dan_PNG_bw6pme.png"
+                  alt="file is loading"
+                ></img>
+              </div>
+              <h6 className="colom6">PENGIRIMAN</h6>
+              <div className="pengiriman">
+                <img
+                  className="gambar3"
+                  src="https://res.cloudinary.com/afsori/image/upload/v1574213276/Arkamedia/kgx-v_csvq95.png"
+                  alt="file is loading"
+                ></img>
+                <img
+                  className="gambar3"
+                  src="https://res.cloudinary.com/afsori/image/upload/v1574187171/Arkamedia/pickupstore_fp6tz6.png"
+                  alt="file is loading"
+                ></img>
+                <img
+                  className="gambar3"
+                  src="https://res.cloudinary.com/afsori/image/upload/v1574187170/Arkamedia/grab_lgyet8.jpg"
+                  alt="file is loading"
+                ></img>
+              </div>
+            </div>
+            <div className="col">
+              <h6 className="colom5">DAPATKAN APPS KAMI</h6>
+              {/* <div className="appkami"> */}
+              <div className="app1">
+                <a href="#!">
+                  <img
+                    className="app1"
+                    src="https://res.cloudinary.com/afsori/image/upload/v1574187170/Arkamedia/play_yah6hh.png"
+                    alt="file is loading"
+                  ></img>
+                </a>
+                <a href="#!">
+                  <img
+                    className="app2"
+                    src="https://res.cloudinary.com/afsori/image/upload/v1574187170/Arkamedia/app_store_x0blqd.png"
+                    alt="file is loading"
+                  ></img>
+                </a>
+              </div>
+              {/* </div> */}
             </div>
           </div>
         </div>
@@ -224,11 +240,10 @@ function Footer() {
         className={"row footer-row"}
         style={{
           height: "2rem",
-          background: "#281e5a",
-          marginTop: "3px"
+          background: "#281e5a"
         }}
       >
-        <p id="copyright">&copy; 2019 Gramedia Digital Nusantara</p>
+        <p id="copyright">&copy; 2019 Arkamedia Digital Nusantara</p>
       </div>
     </div>
   );
