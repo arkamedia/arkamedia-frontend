776092f9fb57bc437cb6e2cc06dbc699d4ace09b
wahyufahrozi <wahyufahrozi97@gmail.com>
2019-11-21 23:28:37 +0700

    Update Component List Content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { ListContent } from "./Components/List_Content/ListContent";
 // import FlashSale from "./Components/Flash_Sale/FlashSale";
 // import { BookPopular } from "./Components/Book_Popular/Book_Popular";
 // import { Choicebrand } from "./Components/Choice_Brand/Choice_Brand";
 // import { EditorChoice } from "./Components/Editor_Choice/EditorChoice";
 // import { Blog } from "./Components/Blog/blog";
-import { Profile } from "./Components/Profile/Profile";
+// import { Biodata } from "./Components/Profile/Biodata";
 
 const App = () => {
   return (
@@ -13,7 +14,8 @@ const App = () => {
       <Choicebrand />
       <EditorChoice /> */}
       {/* <Blog /> */}
-      <Profile />
+      {/* <Biodata /> */}
+      <ListContent />
     </div>
   );
 };
